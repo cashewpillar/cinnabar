f2aa99c8586de4dde07df454ad72dcd042bf0da3
cashewpillar <cashewpillar@gmail.com>
2024-06-29 16:22:24 +0800

    feat: update and delete notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -21,6 +21,26 @@ class NotesController < ApplicationController
     end
   end
 
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+    redirect_to root_path, status: :see_other
+  end
+
   private
     def note_params
       params.require(:note).permit(:title, :body, :status, :reminder)
