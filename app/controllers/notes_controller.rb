08026fc9cb648cdf52c67b1e86ec46685c162649
cashewpillar <cashewpillar@gmail.com>
2024-06-29 16:05:17 +0800

    feat: note validation

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -12,7 +12,7 @@ class NotesController < ApplicationController
   end
 
   def create
-    @note = Note.new(title: "...", body: "...")
+    @note = Note.new(note_params)
 
     if @note.save
       redirect_to @note
@@ -20,4 +20,9 @@ class NotesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  private
+    def note_params
+      params.require(:note).permit(:title, :body, :status, :reminder)
+    end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,8 @@
 class Note < ApplicationRecord
+  validate :title_or_body_required
+
+  private
+  def title_or_body_required
+    errors.add(:title, "or body required") unless title.present? || body.present?
+  end
 end
